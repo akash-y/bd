f059df127543fb88020144fe44f0a8bdbff6c7d3
akash-y <akash.bsy19@gmail.com>
2020-04-03 02:36:03 -0400

    Update BDM_HW3.py

--- BDM_HW3.py
+++ BDM_HW3.py
@@ -1,11 +1,11 @@
 from pyspark import SparkContext
-sc = SparkContext()
 import dateutil
 import csv
-    
-data = sc.textFile(input_file) 
-header = data.first() 
-data = data.filter(lambda x: x != header) \
+
+if __name__=='__main__':
+    data = sc.textFile('/tmp/bdm/complaints.csv') 
+    header = data.first() 
+    data = data.filter(lambda x: x != header) \
         .mapPartitions(lambda x: csv.reader(x, delimiter=',')) \
         .map(lambda x: ((x[1],dateutil.parser.parse(x[0]).year,x[7]),1)) \
         .reduceByKey(lambda x,y: x+y) \
