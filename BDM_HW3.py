239c7849f226969eddc5c01f45bd1b672d0d0453
akash-y <akash.bsy19@gmail.com>
2020-04-07 15:36:18 -0400

    Update BDM_HW3.py

--- BDM_HW3.py
+++ BDM_HW3.py
@@ -23,7 +23,7 @@ if __name__=='__main__':
             .reduceByKey(lambda x,y: x + y) \
             .map(lambda x: (x[0][0], x[0][1], sum(i for i in x[1][1::2]),len(x[1][::2]),
                             (100*max(i for i in x[1][1::2])/sum(i for i in x[1][1::2])))) \
-            .map(lambda x: '\t'.join(x))
+            #.map(lambda x: '\t'.join(x))
             .saveAsTextFile(output_sys)
 
 
