54eb74d9835c441fafd58d90e0fb02926e4595e7
akash-y <akash.bsy19@gmail.com>
2020-04-24 16:35:29 -0400

    Add files via upload

---
+++ HW4.py
@@ -0,0 +1,77 @@
+from pyspark import SparkContext 
+import sys
+import csv
+
+def createIndex(shapefile):
+    '''
+    This function takes in a shapefile path, and return:
+    (1) index: an R-Tree based on the geometry data in the file
+    (2) zones: the original data of the shapefile
+    
+    Note that the ID used in the R-tree 'index' is the same as
+    the order of the object in zones.
+    '''
+    import rtree
+    import fiona.crs
+    import geopandas as gpd
+    zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
+    index = rtree.Rtree()
+    for idx,geometry in enumerate(zones.geometry):
+        index.insert(idx, geometry.bounds)
+    return (index, zones)
+
+def findZone(p, index, zones):
+    '''
+    findZone returned the ID of the shape (stored in 'zones' with
+    'index') that contains the given point 'p'. If there's no match,
+    None will be returned.
+    '''
+    match = index.intersection((p.x, p.y, p.x, p.y))
+    for idx in match:
+        if zones.geometry[idx].contains(p):
+            return idx
+    return None
+
+
+def processTrips(pid, records):
+    '''
+    Our aggregation function that iterates through records in each
+    partition, checking whether we could find a zone that contain
+    the pickup location.
+    '''
+    import csv
+    import pyproj
+    import shapely.geometry as geom
+    
+    # Create an R-tree index
+    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)    
+    index, zones = createIndex(sys.argv[1])   
+    index1, zones1 = createIndex(sys.argv[2])   
+
+    
+    # Skip the header
+    if pid==0:
+        next(records)
+    reader = csv.reader(records)
+    counts = {}
+    
+    for row in reader:
+        #pdt = row[0][5:7]
+        #if pdt !='02': continue # skip all records != around 10:xx pickup
+        p = geom.Point(proj(float(row[6]), float(row[5])))
+        
+        match = None
+        
+        n_zone = zones.boro_name[findZone(p, index, zones)]#, zones1.geometry[findZone(q, index1, zones1)]
+
+        if n_zone:
+            counts[n_zone] = counts.get(n_zone, 0) + 1
+    return counts.items()
+
+if __name__ = 'main':
+    sc = SparkContext()
+    rdd = sc.textFile(sys.argv[3])
+    counts = rdd.mapPartitionsWithIndex(processTrips) \
+            .reduceByKey(lambda x,y: x+y) \
+            .collect()
+    print(counts)
