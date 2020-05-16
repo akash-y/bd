aa722d4497d2d87ea9d9b05fad6d7652cb9017ae
rockymayur <akash.bsy19@gmail.com>
2020-05-16 00:46:46 -0400

    Update pylit.py

--- pylit.py
+++ pylit.py
@@ -23,7 +23,7 @@ def func_slope(v1,v2,v3,v4,v5):
     
     try:
         X = np.array([2015,2016,2017,2018,2019])
-        y = np.array([v1,v2])
+        y = np.array([v1,v2,v3,v4,v5])
     
         m = (((np.mean(X)*np.mean(y)) - np.mean(X*y)) / ((np.mean(X)**2) - np.mean(X*X)))
     
@@ -51,7 +51,6 @@ if __name__ == "__main__":
     
     df_violations = df_violations.withColumn("Street Name",f.upper(f.col("Street Name")))
 
-
     df_violations = df_violations.withColumn("Violation County", translate(boro_dict)("Violation County"))
 
     df_violations = df_violations.withColumn('Issue Date',f.year(f.to_timestamp('Issue Date', 'MM/dd/yyyy')))
@@ -60,12 +59,13 @@ if __name__ == "__main__":
 
     df_violations = df_violations.filter(df_violations['House Number'].rlike('^[0-9]+([ -][0-9]+)?$'))
 
-    df_violations = df_violations.withColumn(regexp_replace(col("House Number"), "-", " ").alias("House Number"))
+    df_violations = df_violations.withColumn("House Number",regexp_replace(col("House Number"), "-", " ").alias("House Number"))
 
-    df_violations = df_violations.withColumn(regexp_replace(col("House Number"), " ", "").alias("House Number"))
+    df_violations = df_violations.withColumn("House Number",regexp_replace(f.col("House Number"), " ", "").alias("House Number"))
 
     df_violations = df_violations.withColumn("Odd_Even",f.when((f.col("House Number")%2==0),"Even").otherwise("Odd"))
 
+                                         
     df_violations.cache()
 
 
@@ -76,27 +76,40 @@ if __name__ == "__main__":
     
     df_centerline_r = df_centerline.select("PHYSICALID","R_LOW_HN","R_HIGH_HN","ST_NAME","FULL_STREE","BOROCODE")
 
-    df_centerline_l = df_centerline_l.filter(df_violations['House Number'].rlike('^[0-9]+([ -][0-9]+)?$'))
 
-    df_centerline_r = df_centerline_r.filter(df_violations['House Number'].rlike('^[0-9]+([ -][0-9]+)?$'))
+    df_centerline_l = df_centerline_l.filter(df_centerline_l['L_LOW_HN'].rlike('^[0-9]+([ -][0-9]+)?$'))
 
-    df_centerline_l = df_centerline_l.withColumn(regexp_replace(regexp_replace(col("L_LOW_HN "), "-", " ")," ","").alias("L_LOW_HN"))
+    df_centerline_l = df_centerline_l.filter(df_centerline_l['L_HIGH_HN'].rlike('^[0-9]+([ -][0-9]+)?$'))
 
-    df_centerline_l = df_centerline_l.withColumn(regexp_replace(regexp_replace(col("L_HIGH_HN "), "-", " ")," ","").alias("L_HIGH_HN"))
+    df_centerline_r = df_centerline_r.filter(df_centerline_r['R_LOW_HN'].rlike('^[0-9]+([ -][0-9]+)?$'))
 
-    df_centerline_r = df_centerline_l.withColumn(regexp_replace(regexp_replace(col("R_HIGH_HN "), "-", " ")," ","").alias("R_HIGH_HN"))
+    df_centerline_r = df_centerline_r.filter(df_centerline_r['R_HIGH_HN'].rlike('^[0-9]+([ -][0-9]+)?$'))
 
-    df_centerline_r = df_centerline_l.withColumn(regexp_replace(regexp_replace(col("R_LOW_HN "), "-", " ")," ","").alias("R_LOW_HN"))
+
+    df_centerline_l = df_centerline_l.withColumn("L_LOW_HN",regexp_replace(regexp_replace(col("L_LOW_HN"), "-", " ")," ","").alias("L_LOW_HN"))
+
+    df_centerline_l = df_centerline_l.withColumn("L_HIGH_HN",regexp_replace(regexp_replace(col("L_HIGH_HN"), "-", " ")," ","").alias("L_HIGH_HN"))
+
+    df_centerline_r = df_centerline_r.withColumn("R_HIGH_HN",regexp_replace(regexp_replace(col("R_HIGH_HN"), "-", " ")," ","").alias("R_HIGH_HN"))
+
+    df_centerline_r = df_centerline_r.withColumn("R_LOW_HN",regexp_replace(regexp_replace(col("R_LOW_HN"), "-", " ")," ","").alias("R_LOW_HN"))
+
+
 
     df_centerline_l=df_centerline_l.withColumn('Odd_Even', lit("Odd"))
 
     df_centerline_r=df_centerline_r.withColumn('Odd_Even', lit("Even"))
 
+                                         
     df_centerline = df_centerline_l.union(df_centerline_r)
 
 
 
-    df_centerline = df_centerline.select('PHYSICALID',col('L_LOW_HN').alias('LOW_HN'),col('L_HIGH_HN').alias('HIGH_HN'),'ST_NAME','FULL_STREE','BOROCODE','Odd_Even')
+    df_centerline = df_centerline.select(col('PHYSICALID'),col('L_LOW_HN').alias('LOW_HN'),col('L_HIGH_HN').alias('HIGH_HN'),'ST_NAME','FULL_STREE','BOROCODE','Odd_Even')
+
+    df_centerline = df_centerline.withColumn("PHYSICALID", df_centerline['PHYSICALID'].cast("int"))
+
+    df_centerline.cache()
 
     final_df = df_violations.join(f.broadcast(df_centerline),
                              [(df_centerline['ST_NAME'] == df_violations['Street Name']) | (df_centerline['FULL_STREE'] == df_violations['Street Name']),
@@ -109,9 +122,11 @@ if __name__ == "__main__":
 
     final_df = final_df.drop(*columns_to_drop)
 
-    final_df = final_df.withColumn('OLS_COEFF', lit(calculate_slope_udf(final_df['2015'],final_df['2016'],final_df['2017'],final_df['2018'],final_df['2019'])))
 
-    final_df = final_df.select('PHYSICALID',col('2015').alias('COUNT_2015'),col('2016').alias('COUNT_2016'),col('2017').alias('COUNT_2017'),col('2018').alias('COUNT_2018'),col('2017').alias('COUNT_2019'),'OLS_COEFF')
+    final_df = final_df.withColumn('OLS_COEFF', lit(calculate_slope_udf(final_df['2016'],final_df['2017'])))
 
+    final_df = final_df.select('PHYSICALID',col('2015').alias('COUNT_2015'),col('2016').alias('COUNT_2016'),col('2017').alias('COUNT_2017'),col('2018').alias('COUNT_2018'),col('2019').alias('COUNT_2019'),'OLS_COEFF')
 
     final_df.show(3000)
+
+    print('elapsed seconds:', time.time() - start_time)
